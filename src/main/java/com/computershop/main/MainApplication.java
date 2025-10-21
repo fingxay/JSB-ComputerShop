a44d438c7bffdd59fdb1ab8553ac242f5cd043d7
hayamij <phuongtuan.dev@gmail.com>
2025-10-21 21:45:27 +0700

    initial commit

---
+++ src/main/java/com/computershop/main/MainApplication.java
@@ -0,0 +1,14 @@
+package com.computershop.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Computer Shop Application...");
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
