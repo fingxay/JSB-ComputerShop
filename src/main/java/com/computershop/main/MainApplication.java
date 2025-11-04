0fc9ff819129073c2f4f9af87c1626998252a89e
hayamij <phuongtuan.dev@gmail.com>
2025-11-04 21:13:04 +0700

    add terminal notification

--- src/main/java/com/computershop/main/MainApplication.java
+++ src/main/java/com/computershop/main/MainApplication.java
@@ -9,6 +9,9 @@ public class MainApplication {
 	public static void main(String[] args) {
 		System.out.println("Starting Computer Shop Application...");
 		SpringApplication.run(MainApplication.class, args);
+		System.out.println("Computer Shop Application Started Successfully.");
+		System.out.println("Access the application at http://localhost:8080");
+		System.out.println("Use default admin credentials - Username: admin | Password: 123456");
 	}
 
 }
